cf6d9b6d0d5430d89637456d54fbeae4cbf19e86
Chris Neitzer <chris@neitzer.com>
2017-01-09 20:49:05 -0500

    Copy the dotfiles first.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cp aliases ~/.aliases
-cp curlrc ~/.curlrc
-cp exports ~/.exports
-cp functions ~/.functions
-cp gitconfig ~/.gitconfig
-cp inputrc ~/.inputrc
-cp jshintrc ~/.jshintrc
-cp nvimrc ~/.nvimrc
+cp dotfiles/aliases ~/.aliases
+cp dotfiles/curlrc ~/.curlrc
+cp dotfiles/exports ~/.exports
+cp dotfiles/functions ~/.functions
+cp dotfiles/gitconfig ~/.gitconfig
+cp dotfiles/inputrc ~/.inputrc
+cp dotfiles/jshintrc ~/.jshintrc
+cp dotfiles/nvimrc ~/.nvimrc
 #cp sublimeconfig ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-cp tmux ~/tmux.conf
-cp vimrc ~/.vimrc
-cp wgetrc ~/.wgetrc
-cp -i bashrc ~/.bashrc
+cp dotfiles/tmux ~/tmux.conf
+cp dotfiles/vimrc ~/.vimrc
+cp dotfiles/wgetrc ~/.wgetrc
+cp -i dotfiles/bashrc ~/.bashrc
 
 if [[ $OSTYPE == darwin* ]]; then
 	OS_STRING = "MacOS"
@@ -27,17 +27,3 @@ else
 		bash init/fedora_postinstall.sh
 	fi
 fi
-
-cp dotfiles/aliases ~/.aliases
-cp dotfiles/curlrc ~/.curlrc
-cp dotfiles/exports ~/.exports
-cp dotfiles/functions ~/.functions
-cp dotfiles/gitconfig ~/.gitconfig
-cp dotfiles/inputrc ~/.inputrc
-cp dotfiles/jshintrc ~/.jshintrc
-cp dotfiles/nvimrc ~/.nvimrc
-#cp sublimeconfig ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-cp dotfiles/tmux ~/tmux.conf
-cp dotfiles/vimrc ~/.vimrc
-cp dotfiles/wgetrc ~/.wgetrc
-cp -i dotfiles/bashrc ~/.bashrc
